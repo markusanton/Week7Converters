5de80be15064d8550a81bc0cd7a7a2ec65086545
markusanton <markusa070@gmail.com>
2020-10-14 15:27:09 +0300

    Add project files.

---
+++ Week7Converters/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Week7Converters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you want to convert celsius to fahrenheit type A.");
+            Console.WriteLine("If you want to convert farenheit to celsius type B.");
+            string userChoice = Console.ReadLine().ToLower();
+            if (userChoice == "a")
+            {
+                CelsiusFahrenheit();
+            }
+            else if (userChoice == "b")
+            {
+                FahrenheitCelsius();
+            }
+            else
+            {
+                Console.WriteLine("Please try again.");
+            }
+        }
+        public static void CelsiusFahrenheit()
+        {
+            Console.WriteLine("Enter your temperature in celsius.");
+            var userInput = Int32.Parse(Console.ReadLine());
+            var fahrenheitResult = (userInput * 9) / 5 + 32;
+            Console.WriteLine($"{userInput} celsius is {fahrenheitResult} fahrenheit.");
+
+        }
+        public static void FahrenheitCelsius()
+        {
+            Console.WriteLine("Enter your temperature in fahrenheit.");
+            var userInput = Int32.Parse(Console.ReadLine());
+            var celsiusResult = (userInput - 32) * 5 / 9;
+            Console.WriteLine($"{userInput} in fahrenheit is {celsiusResult} celsius.");
+        }
+    }
+}
