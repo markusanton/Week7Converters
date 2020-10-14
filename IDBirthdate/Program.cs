197a82c6e8feb36bd25c605282deb3ccd2049a70
markusanton <markusa070@gmail.com>
2020-10-14 16:08:52 +0300

    xd

---
+++ IDBirthdate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace IDBirthdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your personal code:");
+            string userInput = Console.ReadLine();
+
+
+        }
+    }
+}
